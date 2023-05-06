3343c5855f0bebb352da87268b563c3176e5609f
Safiyudeen-dev <132726180+Safiyudeen-dev@users.noreply.github.com>
2023-05-06 08:46:15 +0530

    Update expense.service.ts

--- src/app/shared/services/expense.service.ts
+++ src/app/shared/services/expense.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'environments/environment';
+import { CommonService } from './common.service';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class ExpenseService {
+    constructor(
+        private http: HttpClient,
+        private router: Router,
+        private _commonService: CommonService
+    ) {}
+    env = environment;
 
-  constructor() { }
+    addCategoryImage(value: any) {
+        return this.http.post(this.env.BASE_URL + '/images', value);
+    }
+
+    addCategory(value: any) {
+        return this.http.post(
+            this.env.BASE_URL + '/add/category',
+            { data: this._commonService.encryptData(value) },
+            this._commonService.httpOptions
+        );
+    }
+
+    getCategory(value: any) {
+        return this.http.post(
+            this.env.BASE_URL + '/search/category',
+            { data: this._commonService.encryptData(value) },
+            this._commonService.httpOptions
+        );
+    }
 }
